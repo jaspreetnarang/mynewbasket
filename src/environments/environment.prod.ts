ef4fc0d612d99770417e8698fed94b18f3c9ba7b
Narangjass15 <jaspreetnarang83@gmail.com>
2018-09-20 23:37:42 +0530

    Initial Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBWqXk70msVljfGlYR0wwyC2Y5QadPAe0w",
+    authDomain: "mybasket-f130f.firebaseapp.com",
+    databaseURL: "https://mybasket-f130f.firebaseio.com",
+    projectId: "mybasket-f130f",
+    storageBucket: "mybasket-f130f.appspot.com",
+    messagingSenderId: "818352948214"
+  }
+};
